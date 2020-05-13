6d9a6084975f03b32ba20abd0aa18860f0c1f908
Sky Lin <sky.lin@hulu.com>
2020-05-12 21:50:24 -0700

    Updates

---
+++ precache-manifest.0d30fcb4cad71eefb612275831b5752b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da17aae2ddfac8ed19cfe7b87f34b5b",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "ea4c3f491732b132ae94",
+    "url": "/demo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "46655e9440899b5d6df9",
+    "url": "/demo-app/static/js/2.386c83fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-app/static/js/2.386c83fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4c3f491732b132ae94",
+    "url": "/demo-app/static/js/main.2401884a.chunk.js"
+  },
+  {
+    "revision": "7a9fd2c310f600118c67",
+    "url": "/demo-app/static/js/runtime-main.3fd7ae64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f3f1a3ab399528a66608350d8f215a1.js"
+  "/demo-app/precache-manifest.0d30fcb4cad71eefb612275831b5752b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
